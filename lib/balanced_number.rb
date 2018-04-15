572f23979709d607b9714b7b153e13b660491736
Muzzi Aldean <amiraldean@gmail.com>
2018-04-15 21:02:34 +0100

    Refactor1: ternary operator instead of nested if statements

--- lib/balanced_number.rb
+++ lib/balanced_number.rb
@@ -1,19 +1,9 @@
-# define method to check if a number is balanced
-
 def balancedNum(num)
   num_array =  num.to_s.split("").map(&:to_i)
   i = num_array.length / 2
   if num_array.length.even?
-    if num_array[0...i-1].inject(0, :+) == num_array[i+1..-1].inject(0, :+)
-      "Balanced"
-    else
-      "Not Balanced"
-    end
+    num_array[0...i-1].inject(0, :+) == num_array[i+1..-1].inject(0, :+) ? "Balanced" : "Not Balanced"
   else
-    if num_array[0...i].inject(0, :+) == num_array[i+1..-1].inject(0, :+)
-      "Balanced"
-    else
-      "Not Balanced"
-    end
+    num_array[0...i].inject(0, :+) == num_array[i+1..-1].inject(0, :+) ? "Balanced" : "Not Balanced"
   end
 end
