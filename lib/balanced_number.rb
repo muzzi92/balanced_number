b6e50c2173ad6b69e7066c7d7f74a5782946cc34
Muzzi Aldean <amiraldean@gmail.com>
2018-04-15 20:31:14 +0100

    Tests if 363 is blanced. Passes.

---
+++ lib/balanced_number.rb
@@ -0,0 +1,5 @@
+# define method to check if a number is balanced
+
+def balancedNum(num)
+  "Balanced"
+end
---
+++ spec/balanced_number_spec.rb
@@ -0,0 +1,7 @@
+require "balanced_number"
+
+describe "balancedNum" do
+  it "should return 'Balanced' for 363" do
+    expect(balancedNum(363)).to eq("Balanced")
+  end
+end
