1b6dc0d4b230b46068addf8644671201a2763407
Muzzi Aldean <amiraldean@gmail.com>
2018-04-15 20:36:50 +0100

    Tests if 234 is not balanced. Passes.

--- lib/balanced_number.rb
+++ lib/balanced_number.rb
@@ -1,5 +1,10 @@
 # define method to check if a number is balanced
 
 def balancedNum(num)
-  "Balanced"
+  num_array =  num.to_s.split("").map(&:to_i)
+  if num_array[0] == num_array[2]
+    "Balanced"
+  else
+    "Not Balanced"
+  end 
 end
--- spec/balanced_number_spec.rb
+++ spec/balanced_number_spec.rb
@@ -4,4 +4,8 @@ describe "balancedNum" do
   it "should return 'Balanced' for 363" do
     expect(balancedNum(363)).to eq("Balanced")
   end
+
+  it "should return 'Not Balanced' for 234" do
+    expect(balancedNum(234)).to eq("Not Balanced")
+  end 
 end
